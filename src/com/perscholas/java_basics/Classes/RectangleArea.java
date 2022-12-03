b7cb9214ff7ac4bc09d43320220d5e9b9a183386
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2022-12-03 14:37:19 -0800

    Starting Out with Java From Control Structures through Objects Sixth Edition - chapter 6 - question #3 finished and done right :D

--- src/com/perscholas/java_basics/Classes/RectangleArea.java
+++ src/com/perscholas/java_basics/Classes/RectangleArea.java
@@ -1,24 +1,6 @@
 package com.perscholas.java_basics.Classes;
 import java.util.Scanner;
 
-    /*
-        If you have downloaded the book’s source code from www.pearsonhighered.com/gaddis,
-    you will find a partially written program named AreaRectangle.java in this chapter’s
-    source code folder. Your job is to complete the program.
-
-    When it is complete, the program will ask the user to enter the width and length of a rectangle, and then display
-     the rectangle’s area.
-
-     The program calls the following methods, which have not been written:
-    •	 getLength—This method should ask the user to enter the rectangle’s length, and then
-    return that value as a double.
-    •	 getWidth—This method should ask the user to enter the rectangle’s width, and then
-    return that value as a double.
-    •	 getArea—This method should accept the rectangle’s length and width as arguments, and
-    return the rectangle’s area. The area is calculated by multiplying the length by the width.
-    •	 displayData—This method should accept the rectangle’s length, width, and area as
-    arguments, and display them in an appropriate message on the screen.
-     */
 
 
 public class RectangleArea
@@ -94,7 +76,7 @@ public class RectangleArea
     }
 
     /**
-     The displayData displays a rectanlge's length, width,
+     The displayData displays a rectangle's length, width,
      and area, formatted with 1 digit after the decimal point.
      @param length The rectangle's length.
      @param width The rectangle's width.
